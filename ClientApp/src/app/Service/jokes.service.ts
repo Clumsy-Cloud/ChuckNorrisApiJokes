949d845079deb60ddadff1d7dc19ac8ceecc9989
Adrian Thurston <A.b.thurston007@gmail.com>
2021-01-06 11:45:03 +0200

    Chuck norris API and frontend done

---
+++ ClientApp/src/app/Service/jokes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
+
+import{Joke} from '../Model/joke.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokesService {
+  private apiUrl = 'http://localhost:43535/jokes/';
+  //https://api.chucknorris.io/jokes
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // Get Random Joke
+  getRandomJoke(): Observable<Joke> {
+    return this.http.get<Joke>(this.apiUrl + 'random');
+  }
+
+  // Get Categories
+  getCategories() {
+    var a =this.http.get<string[]>(this.apiUrl +  'categories');
+    return a;
+  }
+
+  // Get joke by Category
+  getCategoryJoke(category: string){
+    return this.http.get<Joke>(this.apiUrl + `random?category=${category}`);
+  }
+
+  // Get Search Jokes
+  getSearchJokes(searchTerm : string) {
+    return  this.http.get<{result: Joke[], amount:  number}>(this.apiUrl + `/search?query=${searchTerm}`);
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -67,7 +67,7 @@ namespace ChuckNorrisJokes
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
